b88cb4be38627ddd0cf1177982d2391cd7abe090
Evan Bigbie <wbigbie@gmail.com>
2020-02-10 18:42:56 -0600

    condition + qty + media object (no image yet)

--- functions/prodadmin/js/add_page.js
+++ functions/prodadmin/js/add_page.js
@@ -16,6 +16,10 @@ function add_page_secured() {
             <p id="name_error" style="color:red;" />
         </div>
         <div class="form-group">
+            Condition: <input class="form-control" type="text" id="condition" />
+            <p id="condition_error" style="color:red;" />
+        </div>
+        <div class="form-group">
             Summmary: <br>
             <textarea class="form-control" id="summary" cols="40" rows="5"></textarea>
             <p id="summary_error" style="color:red;" />
@@ -23,6 +27,10 @@ function add_page_secured() {
         <div class="form-group">
             Price: <input class="form-control" type="text" id="price" />
             <p id="price_error" style="color:red;" />
+        </div>
+        <div class="form-group">
+            Quantity: <input class="form-control" type="text" id="quantity" />
+            <p id="quantity_error" style="color:red;" />
         </div>
         <div class="form-group">
             Image: <input type="file" id="imageButton" value="upload" />
@@ -41,22 +49,28 @@ function add_page_secured() {
 
 async function addProduct() {
     const name = document.getElementById('name').value
+    const condition = document.getElementById('condition').value
     const summary = document.getElementById('summary').value
     let price = document.getElementById('price').value
+    let quantity = document.getElementById('quantity').value
 
     // input validation
     const nameErrorTag = document.getElementById('name_error')
+    const conditionErrorTag = document.getElementById('condition_error')
     const summaryErrorTag = document.getElementById('summary_error')
     const priceErrorTag = document.getElementById('price_error')
+    const quantityErrorTag = document.getElementById('quantity_error')
     const imageErrorTag = document.getElementById('image_error')
 
     nameErrorTag.innerHTML = validate_name(name)
+    conditionErrorTag.innerHTML = validate_condition(condition)
     summaryErrorTag.innerHTML = validate_summary(summary)
     priceErrorTag.innerHTML = validate_price(price)
+    quantityErrorTag.innerHTML = validate_quantity(quantity)
     imageErrorTag.innerHTML = !glImageFile2Add ? 'Error: image file not selected' : null
 
-    if (nameErrorTag.innerHTML || summaryErrorTag.innerHTML 
-        || priceErrorTag.innerHTML || imageErrorTag.innerHTML) {
+    if (nameErrorTag.innerHTML || conditionErrorTag.innerHTML || summaryErrorTag.innerHTML 
+        || priceErrorTag.innerHTML || quantityErrorTag.innerHTML || imageErrorTag.innerHTML) {
             return
         }
 
@@ -70,14 +84,27 @@ async function addProduct() {
 
         // product: name, summary, price in Firestore
         price = Number(price)
+        quantity = Number(quantity)
         await firebase.firestore().collection(COLLECTION).doc()
-            .set({name, summary, price, image, image_url})
+            .set({name, condition, summary, price, quantity, image, image_url})
+
+        // // console.log('image_url', image_url)
+        // glPageContent.innerHTML = `
+        //     <h1>${name} is added</h1>
+        //     <h3>Condition: ${condition}<br />Qty: ${quantity}</h3>
+        //     <a href="/show" class"btn btn-outline-primary">Show all</a>
+        // `;
 
         // console.log('image_url', image_url)
         glPageContent.innerHTML = `
-            <h1>${name} is added</h1>
-            <a href="/show" class"btn btn-outline-primary">Show all</a>
-        `;
+        <div class="media">
+            <img src="image_url" class="mr-3" alt="xxx">
+            <div class="media-body">
+                <h5 class="mt-0">${name} is added</h5>
+                Condition: ${condition} <br />Qty: ${quantity}
+            </div>
+        </div>
+    `;
 
     } catch (e) {
         // JSON.stringify() makes sure that whatever it is it will print to screen
--- functions/prodadmin/js/product_validate.js
+++ functions/prodadmin/js/product_validate.js
@@ -5,6 +5,18 @@ function validate_name(name) {
     return null
 }
 
+/////  video:
+
+function validate_condition(condition) {
+  var cond = condition.toLowerCase()
+  if ((cond.includes("new") && cond.trim().length == 3) 
+      || (cond.includes("open box") && cond.trim().length == 8) 
+      || cond.includes("used") && cond.trim().length == 4)
+    return null
+  else
+    return 'Error: Please Type \'New\', \'Open Box\', or \'Used\''
+}
+
 function validate_summary(summary) {
   if (!summary || summary.length < 5)
     return 'Error: min 5 chars'
@@ -16,6 +28,13 @@ function validate_summary(summary) {
 function validate_price(price) {
   if (!parseFloat(price))
     return `Error: Invalid price value ${price}`
+  else
+    return null
+}
+
+function validate_quantity(quantity) {
+  if (!parseFloat(quantity))
+    return `Error: Invalid quantity value ${quantity}`
   else
     return null
 }
